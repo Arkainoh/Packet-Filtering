98bfe6c86f1caefa92638b36b601220c0851430c
Arkainoh <arkainoh@gmail.com>
2016-11-20 18:09:56 +0900

    Implement R/W

--- client_app.c
+++ client_app.c
@@ -11,10 +11,11 @@
 
 int main(int argc, char *argv[]) {
 	
-	char buffer[BUF_LEN];
+	char rbuffer[BUF_LEN];
+	char wbuffer[BUF_LEN];
 	struct sockaddr_in server_addr, client_addr;
 	int client_fd;
-	int len, msg_size;
+	int msg_size;
 
 	if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		printf("Can't open stream socket\n");
@@ -27,9 +28,23 @@ int main(int argc, char *argv[]) {
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	if(connect(client_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
-		printf("Connection failed");
+		printf("Connection failed\n");
 		exit(0);
 	}
+	
+	printf("Message to server:\n");
+	fgets(wbuffer, sizeof(wbuffer), stdin);
+	wbuffer[strlen(wbuffer)-1] = '\0';
+	write(client_fd, wbuffer, strlen(wbuffer));
+		
+	if(msg_size = read(client_fd, rbuffer, sizeof(rbuffer)) == -1) {
+		printf("Read failed.\n");
+	} else {
+		printf("Server : ");
+		puts(rbuffer);
+	}
+	
 
+	close(client_fd);
   return 0;
 }
