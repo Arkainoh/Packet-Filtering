ba376b7d9e0ebfb29a50e7d1da14ab84577c88e7
Arkainoh <arkainoh@gmail.com>
2016-11-24 00:06:57 +0900

    Modify comments and indentations

--- packet_collector/client.c
+++ packet_collector/client.c
@@ -15,6 +15,7 @@
 #define OUTPUT_DIR "output"
 
 void* connect_server(void *data) {
+
 	struct sockaddr_in server_addr;
 	struct timeval start_time, received_time;
 	int client_socket, port=*((int *)data);
@@ -24,7 +25,7 @@ void* connect_server(void *data) {
 	FILE * outputfile;
 	struct tm *tm;
 									
-	//build socket
+	//create a socket
 	client_socket=socket(PF_INET, SOCK_STREAM, 0);
 	
 	if(client_socket==-1) {
@@ -32,34 +33,36 @@ void* connect_server(void *data) {
 		exit(1);
 	}
 											
-	//connect server
+	//connect to the server
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family=AF_INET;
 	server_addr.sin_port=htons(port);
 	server_addr.sin_addr.s_addr=inet_addr(SERVER_ADDR);
 	
-	if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr))<0)
-	{
+	if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr))<0)	{
 		printf("Port #%d : Connection failed.\n", port);
 		exit(1);
 	}
 																
-	//receive and write data
+	//open a file to write data
 	sprintf(outputfiledir, "/%d.txt", port);
 	strcat(outputfilepath, outputfiledir);
 	outputfile=fopen(outputfilepath, "w");
-																
+	
+	//record the current time & initialize packet arrival time
 	gettimeofday(&start_time, NULL);
 	gettimeofday(&received_time, NULL);
 
-	while(received_time.tv_sec-start_time.tv_sec < 3) {
+	//write contents and arrival times of packets to the file for 3 seconds
+	while(received_time.tv_sec-start_time.tv_sec < 3) { 
 	  read(client_socket, buffer, BUF_LEN);
 		gettimeofday(&received_time, NULL);
 		tm=localtime(&received_time.tv_sec);
 		fprintf(outputfile, "%02d:%02d:%02d.%03ld %ld %s\n",
 				tm->tm_hour, tm->tm_min, tm->tm_sec, received_time.tv_usec/1000, strlen(buffer), buffer);
 	}
-												
+	
+	//close the file and quit function after 3 seconds
 	fclose(outputfile);
 	return 0;
 }
@@ -67,13 +70,20 @@ void* connect_server(void *data) {
 int main() {
 	pthread_t p_thread[5];
 	int thr_id, status, port_number[5] = {5555,6666,7777,8888,9999}, i;
+
+	//make a directory to store output files
 	mkdir(OUTPUT_DIR, 0755);
-	for(i=0;i<5;i++) {
+
+	//create 5 threads which call connect_server function with respective port number
+	for(i=0;i<5;i++)
 		thr_id = pthread_create(&p_thread[i], NULL, connect_server, (void *)&port_number[i]);
-	}
-						    
+	
+	
+	//quit 5 threads
 	for(i=0;i<5;i++) {
 		pthread_join(p_thread[i], (void **) &status);
+		if(status != 0)
+			printf("An error while joining a thread\n");
 	}
 										
 	return 0;
